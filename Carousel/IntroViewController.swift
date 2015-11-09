3e43e18902ca3256c9e9373f3a0cced625b9ba5a
Janakd <janakd@gmail.com>
2015-11-08 20:05:34 -0800

    With Transforms
    With gif and optional transforms

--- Carousel/IntroViewController.swift
+++ Carousel/IntroViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController,UIScrollViewDelegate {
 
+    @IBOutlet weak var tile1View: UIImageView!
+    @IBOutlet weak var tile2View: UIImageView!
+    @IBOutlet weak var tile3View: UIImageView!
+    @IBOutlet weak var tile4View: UIImageView!
+    @IBOutlet weak var tile5View: UIImageView!
+    @IBOutlet weak var tile6View: UIImageView!
+    
     @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var imageView: UIImageView!
     
+    var yOffsets : [Float] = [-285, -240, -415, -408, -480, -500]
+    var xOffsets: [Float] = [-30, 75, -66, 10, -200, -15]
+    var scales : [Float] = [1, 1.65, 1.7, 1.6, 1.65, 1.65]
+    var rotations : [Float] = [-10, -10, 10, 10, 10, -10]
+    
+    func convertValue(value: Float, r1Min: Float, r1Max: Float, r2Min: Float, r2Max: Float) -> Float {
+        var ratio = (r2Max - r2Min) / (r1Max - r1Min)
+        return value * ratio + r2Min - r1Min * ratio
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-scrollView.contentSize = (imageView!.image?.size)!
+        scrollView.contentSize = (imageView!.image?.size)!
+        
+        scrollView.delegate = self
         
         // Do any additional setup after loading the view.
     }
@@ -27,6 +47,63 @@ scrollView.contentSize = (imageView!.image?.size)!
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var offset = Float(scrollView.contentOffset.y)
+        
+        var tx = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -30, r2Max: 0)
+        var ty = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -285, r2Max: 0)
+        var scale = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1.3, r2Max: 1)
+        var rotation = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -10, r2Max: 0)
+        
+        tile1View.transform = CGAffineTransformMakeTranslation(CGFloat(tx), CGFloat(ty))
+        tile1View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scale), CGFloat(scale))
+        tile1View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation) * M_PI / 180))
+        
+        var tx1 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 75, r2Max: 0)
+        var ty1 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -240, r2Max: 0)
+        var scalea = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1.65, r2Max: 1)
+        var rotation1 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 50, r2Max: 0)
+        
+        tile2View.transform = CGAffineTransformMakeTranslation(CGFloat(tx1), CGFloat(ty1))
+        tile2View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scalea), CGFloat(scalea))
+        tile2View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation1) * M_PI / 180))
+        
+        var tx2 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -66, r2Max: 0)
+        var ty2 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -200, r2Max: 0)
+        var scaleb = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1.7, r2Max: 1)
+        var rotation2 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 10, r2Max: 0)
+        
+        tile3View.transform = CGAffineTransformMakeTranslation(CGFloat(tx2), CGFloat(ty2))
+        tile3View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scaleb), CGFloat(scaleb))
+        tile3View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation2) * M_PI / 180))
+        
+        var tx3 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 10, r2Max: 0)
+        var ty3 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -250, r2Max: 0)
+        var scalec = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1.65, r2Max: 1)
+        var rotation3 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 10, r2Max: 0)
+        
+        tile4View.transform = CGAffineTransformMakeTranslation(CGFloat(tx3), CGFloat(ty3))
+        tile4View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scalec), CGFloat(scalec))
+        tile4View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation3) * M_PI / 180))
+        
+        var tx4 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -200, r2Max: 0)
+        var ty4 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -240, r2Max: 0)
+        var scaled = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1, r2Max: 1.65)
+        var rotation4 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -10, r2Max: 0)
+        
+        tile5View.transform = CGAffineTransformMakeTranslation(CGFloat(tx4), CGFloat(ty4))
+        tile5View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scaled), CGFloat(scaled))
+        tile5View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation4) * M_PI / 180))
+        
+        var tx5 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -15, r2Max: 0)
+        var ty5 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -540, r2Max: 0)
+        var scalee = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 1.65, r2Max: 1.65)
+        var rotation5 = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: 30, r2Max: 0)
+        
+        tile6View.transform = CGAffineTransformMakeTranslation(CGFloat(tx5), CGFloat(ty5))
+        tile6View.transform = CGAffineTransformScale(tile1View.transform, CGFloat(scalee), CGFloat(scalee))
+        tile6View.transform = CGAffineTransformRotate(tile1View.transform, CGFloat(Double(rotation5) * M_PI / 180))
+    }
 
     /*
     // MARK: - Navigation
